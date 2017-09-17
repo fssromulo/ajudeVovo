f587469eb8d96458702d1eea86aef2cdcbd4b269
jvvppereira <joao.paes@philips.com>
2017-09-17 17:43:41 -0300

    Melhorias avaliação e adição da tela de busca de serviços do lado do cliente

---
+++ application/views/DetalheServico.php
@@ -0,0 +1,31 @@
+<html lang="pt_BR">
+<head>
+	<title>Opa! Ajude o Vovô</title>
+	<!-- jQuery & Bootstrap -->
+
+	<link href="../includes/bootstrap-3.3.7/css/bootstrap-theme.min.css"  type="text/css" rel="stylesheet" />
+	<link href="../includes/bootstrap-3.3.7/css/bootstrap.min.css"  type="text/css" rel="stylesheet" />
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../includes/star-rating/css/star-rating.min.css" media="all" type="text/css"/>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="../includes/star-rating/js/star-rating.min.js" type="text/javascript"></script>
+	<link rel="stylesheet" href="../includes/css/hero.css">
+</head>
+
+<body ng-app="appAngular" ng-controller="controllerAngular">
+	<div class="container">
+		<div class="row">
+			foooi
+        </div>
+    </div>
+    <!-- Angular JS -->
+	<script type="text/javascript" src="../includes/angular/angular.min.js"></script>  
+
+	<link rel='stylesheet' href='../includes/js/angular-loading-bar/build/loading-bar.min.css' type='text/css' media='all' />
+	<script type='text/javascript' src='../includes/js/angular-loading-bar/build/loading-bar.min.js'></script>
+
+	<!-- MY App -->
+	<script type="text/javascript" src="../includes/js/ConsultaServicoCliente.controller.js"></script>
+</body>
+</html>
