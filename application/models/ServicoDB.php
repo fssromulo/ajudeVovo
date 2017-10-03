01fb881b4b7978b27c61c0fa6a628d608464e2d2
jvvppereira <joao.paes@philips.com>
2017-10-02 22:17:30 -0300

    Ajustes funcionamento do carregamento dos corações e informações na tela

--- application/models/ServicoDB.php
+++ application/models/ServicoDB.php
@@ -61,20 +61,25 @@ class ServicoDB extends CI_Model {
 
     public function get_servicos_cliente() {
         return $this->db->query("
-        select	d.descricao ds_categoria, 
-                d.imagem_categoria url_img_categoria,
-                a.nome nm_prestador, 
-                1 qt_estrela, 
-                1 qt_servico, 
-                RPAD(concat_ws(' - ', c.descricao, c.detalhe), 255, ' ') ds_detalhe, 
-                c.valor
-        from	pessoa_fisica a,
-                prestador b,
-                servico c,
-                categoria d
-        where	a.id_pessoa_fisica = b.id_pessoa
-        and		b.id_prestador = c.id_prestador
-        and		c.id_categoria = d.id_categoria ", FALSE);
+        select
+            d.descricao ds_categoria, 
+            d.imagem_categoria url_img_categoria,
+            a.nome nm_prestador, 
+            obter_avaliacao(id_servico) qt_estrela, 
+            obter_quantidade_servicos(id_servico) qt_servico, 
+            RPAD(concat_ws(' - ', c.descricao, c.detalhe), 255, ' ') ds_detalhe, 
+            c.valor
+        from
+        	pessoa_fisica a,
+            prestador b,
+            servico c,
+            categoria d
+        where	
+            a.id_pessoa_fisica = b.id_pessoa
+        and		
+            b.id_prestador = c.id_prestador
+        and		
+            c.id_categoria = d.id_categoria ", FALSE);
     }
 }
 ?>
