421bb919d69c16c3bf014063df2413e1e410281f
romulofssGit <fssromulo@gmail.com>
2017-10-31 23:03:44 -0200

    Ajuste para telas ficarem responsivas e ajuste para bloquear inumeras requisicoes simultanea para o pagSEGURO

--- application/views/testepagseguro.php
+++ application/views/testepagseguro.php
@@ -3,6 +3,9 @@
 <head>
 	<title>CodeIgniter & AngularJS</title>
 	<!-- jQuery & Bootstrap -->
+	    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="../includes/bootstrap-3.3.7/css/bootstrap-theme.min.css"  type="text/css" rel="stylesheet" />
 	<link href="../includes/bootstrap-3.3.7/css/bootstrap.min.css"  type="text/css" rel="stylesheet" />
 </head>
