430fc514dbacef4639000eddf2c869eef42578c5
romulofssGit <fssromulo@gmail.com>
2018-02-26 20:59:14 -0300

    Novas telas Home e Login!

--- application/views/home.php
+++ application/views/home.php
@@ -3,198 +3,181 @@
     $this->load->view('nucleo/header.php');
 ?>
 
+  <!-- CSS  -->
+<!--   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
+
 </head>
-<body class="blue-grey lighten-4">
-    <div data-ng-app="AppHome" data-ng-controller="controllerHome">
-    <div class="container">
-        <!-- Navigation -->
-<!--         <nav>
-            <div class="nav-wrapper blue #1889ff">
-              <a href="#!" class="brand-logo"><img src="../includes/imagens/pwa_icons/android-chrome-48x48.png"/></a>
-              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-              <ul class="right hide-on-med-and-down">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Quero ajudar</a></li>
-                <li><a href="#">Solicitar ajuda</a></li>
-              </ul>
-                  <ul id="mobile-demo" class="side-nav">
-                    <li><div class="user-view">
-                      <div class="background blue #1889ff">
-                        <div class="blue lighten-1"></div>
-                      </div>
-                      <a href="#!user"><img class="circle" src="../includes/imagens/pwa_icons/android-chrome-192x192.png"></a>
-                      <a href="#!name"><span class="white-text name">Adalberto Azevedo</span></a>
-                      <a href="#!email"><span class="white-text email">ajudevovo@gmail.com</span></a>
-                    </div></li>
-                    <li><a href="#" id="link_frm_login" ><i class="material-icons">home</i>Home</a></li>
-                    
-                    <li><a href="#"><i class="material-icons">power_settings_new</i>Sair</a></li>
-                  </ul>
-            </div>
-        </nav> -->
+<body>
+  <nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">
+      	Ajude o Vovô
+      </a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Sobre Nós</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
 
-            <div class="center row" >
-                    <div class="center col s12 m8 offset-m4 l9 offset-l3"> <!-- Note that "m8 l9" was added -->     
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Sobre nós</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
 
-                        <div id="frm_login">
-                           <div class="row">
-                            <div class="row"> &nbsp;</div>
-                            <form class="center col s12 m8 l8">
-                                <div class="">
-                                    <img
-                                        class=" center-align responsive-img" 
-                                        src="../includes/imagens/logo_login.png"
-                                        alt="logo_ajude_vovo"
-                                    />
-                                    <br/>
-                                </div>
-
-                                  <div class="row">
-                                    <div class="input-field col s12">
-                                      <input
-                                        value=""
-                                        type="email"
-                                        class="validate" 
-                                        data-ng-model="usuario_logar"/>
-                                      <label for="disabled">Usuário/E-mail</label>
-                                    </div>
-                                  </div>
-                                  <div class="row">
-                                    <div class="input-field col s12">
-                                      <input
-                                        id="password"
-                                        type="password"
-                                        class="validate"
-                                        data-ng-model="senha_logar"
-                                      />
-                                      <label for="password">Senha</label>
-                                    </div>
-                                  </div>
-                                  <div class="row">
-                                        <div class=" left-align  col s12">
-                                          <a class="left-align" href="#">Novo por aqui? Cadastre-se!</a>
-                                        </div>
-                                  </div>
-                                  <div class="row">
-                                    <div class="input-field col s12 ">
-                                      <a class="waves-effect waves-light btn right  light-blue darken-2">Entrar</a>
-                                    </div>
-                                  </div>
-                            </form>
-                          </div>
-                        </div>
-
-                    </div>
-              </div>
-
-        <!-- nav class="navbar navbar-inverse navbar-fixed-top bg-ajudeVovo" role="navigation">
-            <div class="container">
-
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menuResponsivo">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="">Ajude o vovô</a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="menuResponsivo" >
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <a href="#about">Home</a>
-                        </li>
-                       
-    				    <li>
-               			  <a
-                             href="#"  
-                            popover
-               				data-placement="bottom"
-               				data-toggle="popover"
-               				data-title="Efetue o login"
-               				data-container="body"
-    		             	data-html="true"                             
-    		             	id="login"
-    		             	class="links_menu"
-    		             	data-ng-click="escolherPerfil('ajudante')"
-    			             >Quero Ajudar</a>
-                    	</li>
-
-
-                        <li>
-    						<a href="#" popover
-    							data-placement="bottom"
-    							data-toggle="popover2"
-    							data-title="Efetue o login"                                         
-    							data-container="body"
-    		             	    data-html="true" id="login"
-    		             	    class="links_menu"
-    		             	    data-ng-click="escolherPerfil('contratante')"
-    				        >Solicitar ajuda</a>
-                        </li>
-                    </ul>
-                </div>
-               
-            </div>
-
-        </nav> 
-
-        <div id="popLoginPessoa" class="hide">
-            <div class="form-group">
-                <div class="row">
-                    <div class="col-sm-12">
-                    <input type="text" placeholder="Usuário" data-ng-model="usuario_logar" class="form-control" maxlength="15"><br/>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <input type="password" placeholder="Senha" data-ng-model="senha_logar" class="form-control"><br/>
-                    </div>  
-                </div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <button type="button" data-ng-click="cancelar()" class="btn btn-danger">
-                            Cancelar
-                        </button>
-                        <button type="submit" class="btn btn-primary" data-ng-click="fazerLogin()">
-                            Login
-                        </button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="left-align col-sm-12">
-                        <a
-                        href="{{link_cadastro}}"
-                        class="left-align">Novo por aqui? Cadastre-se!</a>
-                        <br/>
-                    </div>
-                </div>                
-            </div>
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-2">Ajude o Vovô</h1>
+        <div class="row center">
+          <h5 class="header col s12 light">Ajude um idoso ou seja ajudado!</h5>
         </div>
-    -->
-        
+        <div class="row center">
+          <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Começar</a>
+        </div>
+        <br><br>
+
+      </div>
     </div>
+    <div class="parallax"><img src="../includes/imagens/inicial/maos.png" alt="maos"></div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">important_devices</i></h2>
+            <h5 class="center">Quem somos</h5>
+
+            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">favorite_border</i></h2>
+            <h5 class="center">Nosso objetivo</h5>
+
+            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+            <h5 class="center">Como funciona?</h5>
+
+            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
-    <script type="text/javascript" src="../includes/jQuery/jquery-3.2.1.js"></script>
-
-<!-- 
-
-	<script type="text/javascript" src="../includes/bootstrap-3.3.7/js/bootstrap.min.js"></script>  
+  </div>
 
 
-	<script type="text/javascript" src="../includes/angular/angular.min.js"></script>  
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="../includes/imagens/inicial/opa.png" alt="Unsplashed background img 2"></div>
+  </div>
 
-	<link rel='stylesheet' href='../includes/js/angular-loading-bar/build/loading-bar.min.css' type='text/css' media='all' />
-	<script type='text/javascript' src='../includes/js/angular-loading-bar/build/loading-bar.min.js'></script>
- -->
-    <?php
+  <div class="container">
+    <div class="section">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Contact Us</h4>
+          <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><!-- <img src="background3.jpg" alt="Unsplashed background img 3"> --></div>
+  </div>
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <?php
         // Importa o cabeçalho rodape padrao a todas as telas
         $this->load->view('nucleo/footer.php');
     ?> 
+    <script type="text/javascript">
+                
+        $( document ).ready(function() {
+
+          $('.button-collapse').sideNav();
+          $('.parallax').parallax();
+        });
+    </script>
+
+  <!--  Scripts-->
+  <!-- <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+  <!-- <script src="js/materialize.js"></script> -->
+  <!-- <script src="js/init.js"></script> -->
+
+  </body>
 
 
-	<script type="text/javascript" src="../includes/js/home.js?<?php echo date('YmdHis');?>"></script>
-</body>
 
 </html>
