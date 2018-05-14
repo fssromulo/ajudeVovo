dd651636dd78726a5b9b28d217239b0af1c586d1
jvvppereira <joao.paes@philips.com>
2018-05-14 13:50:19 -0300

    Removido diretiva não mais utilizada

--- application/views/ControlePrestador.php
+++ application/views/ControlePrestador.php
@@ -16,7 +16,7 @@
     <input type="hidden" ng-model="is_contratante" name="is_contratante" ng-init="is_contratante=1" />
     <div class="container" data-ng-show="arrListaServico.length > 0">
          <ul  class="collapsible" data-collapsible="accordion" >
-            <li ng-repeat="lista in arrListaServico" after-load-services-directive>
+            <li ng-repeat="lista in arrListaServico">
                 <div class="collapsible-header" ng-click="openServiceClick($event)">
                     <img alt="" data-ng-src="../includes/imagens/fotos_pessoas/{{lista.imagem_pessoa}}" class="circle " width="50" height="50"> &nbsp;
                         <p class="truncate">{{lista.descricao}}</p> 
