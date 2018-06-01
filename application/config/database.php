1ea7b94d3e527f059513758ba8d40f45146aaef0
romulofssGit <fssromulo@gmail.com>
2018-06-01 18:32:34 -0300

    Ajustes gerias

--- application/config/database.php
+++ application/config/database.php
@@ -118,3 +118,25 @@ $db['producao'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['bancoProducaoAWS'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => 'root',
+	'database' => 'ajudevovo',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
